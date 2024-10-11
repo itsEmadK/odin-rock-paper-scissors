f45555ccb5be9619a6f3d8a5545885ec9e74cd82
Emad <67466634+itsEmadK@users.noreply.github.com>
2024-10-11 21:24:12 +0330

    Add logging the result of the round

--- script.js
+++ script.js
@@ -19,14 +19,32 @@ function getUsersChoice() {
 //returns the winner of the round.
 function playRound(usersChoice,computersChoice) {
     if (usersChoice == computersChoice) {
+        console.log(
+            `You both used ${usersChoice} so this is a tie!`
+        );
+        
         return null;
     }else if (
         String(usersChoice).toLowerCase() == "rock" && computersChoice == "scissors" ||
         String(usersChoice).toLowerCase() == "paper" && computersChoice == "rock" ||
         String(usersChoice).toLowerCase() == "scissors" && computersChoice == "paper"
     ) {
+        console.log(`Your ${usersChoice} ${
+            usersChoice === "rock" ? "broke" :
+            usersChoice === "paper" ? "covered" : "cut"
+        } computer's ${computersChoice}!
+        `);
+        
         return "user";
     }else {
+        console.log(`Your ${usersChoice} ${
+            usersChoice === "rock" ? "was covered" :
+            usersChoice === "paper" ? "was cut" : "were broken"
+        } by computer's ${computersChoice}`
+    );
         return "computer";
     }
 }
+
+
+playRound(getUsersChoice(), getComputersChoice());
