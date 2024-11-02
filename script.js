0f4309f36b4610cf4b835655b8744a6a6afd1710
Emad <67466634+itsEmadK@users.noreply.github.com>
2024-11-02 15:58:58 +0330

    Update playRound func so it only calculates round winner without showing any messages in console

--- script.js
+++ script.js
@@ -49,40 +49,19 @@ function getComputersChoice() {
 
 //returns the winner of the round.
 function playRound(usersChoice, computersChoice) {
-    if (usersChoice ?? "".toLowerCase() === computersChoice.toLowerCase()) {
-        console.log(
-            `You both used ${usersChoice} so this is a tie!`
-        );
-
+    if (usersChoice.toLowerCase() === computersChoice.toLowerCase()) {
         return null;
     } else if (
         String(usersChoice).toLowerCase() === "rock" && computersChoice === "scissors" ||
         String(usersChoice).toLowerCase() === "paper" && computersChoice === "rock" ||
         String(usersChoice).toLowerCase() === "scissors" && computersChoice === "paper"
     ) {
-        console.log(`Your ${usersChoice} ${usersChoice === "rock" ? "broke" :
-            usersChoice === "paper" ? "covered" : "cut"
-        } computer's ${computersChoice}!
-            `);
-
         return "user";
-    } else {
-
-        if (
-            String(usersChoice).toLowerCase() === "rock" ||
-            String(usersChoice).toLowerCase() === "paper" ||
-            String(usersChoice).toLowerCase() === "scissors"
-        ) {
-            console.log(`Your ${usersChoice} ${usersChoice === "rock" ? "was covered" :
-                    usersChoice === "paper" ? "was cut" : "were broken"
-                } by computer's ${computersChoice}`
-            );
-        } else {
-            console.log(usersChoice == null ? "You forfeited the round!" :
-                "You got disqualified this round due to using an unknown object!")
-            ;
-        }
-
+    } else if (
+        String(usersChoice).toLowerCase() === "rock" ||
+        String(usersChoice).toLowerCase() === "paper" ||
+        String(usersChoice).toLowerCase() === "scissors"
+    ) {
         return "computer";
     }
 }
